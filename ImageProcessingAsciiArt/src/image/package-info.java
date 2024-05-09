f7f621b8528ee1ebd5d7984bd6241dbb42bfdb46
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2024-05-09 15:45:15 +0300

    Add files via upload

---
+++ ImageProcessingAsciiArt/src/image/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Provides comprehensive image processing functionalities, including calculating brightness of
+ * sub-images, managing images, padding images to the next power of two, and extracting sub-images.
+ * This package is designed to facilitate advanced operations on images for applications that require
+ * detailed image analysis and manipulation.
+ * Key functionalities include:
+ * Calculating the average brightness of sub-images based on a defined formula, useful for image analysis
+ * and processing tasks where brightness levels are critical.
+ * Initializing and managing images, allowing for operations such as loading from files, saving, and
+ * accessing pixel data, providing a foundation for further image processing tasks.
+ * Padding images to the next power of two, a common requirement for many graphical processing algorithms
+ * and operations, ensuring compatibility and optimization for various processing techniques.
+ * Extracting sub-images, enabling detailed analysis and manipulation of specific regions within
+ * larger images, supporting tasks such as texture analysis, pattern recognition, and more.
+ * This package is essential for developers working on applications requiring detailed and
+ * sophisticated image processing capabilities, offering a robust set of tools for a wide range of
+ * image manipulation tasks.
+ *
+ * @author Noam Shabat and Samuel Hayat
+ * @version Java 11
+ */
+package image;
