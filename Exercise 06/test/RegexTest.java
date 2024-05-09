f6dc56f30cebd8574e4ed28ab97aaf01453de3da
Noam Shabat <92057540+Noamshabat1@users.noreply.github.com>
2024-05-09 20:31:54 +0300

    Delete Exercise 06 directory

--- Exercise 06/test/RegexTest.java
+++
@@ -1,9 +0,0 @@
-import oop.ex6.utils.RegexConstants;
-
-public class RegexTest {
-
-    public static void main(String[] args) {
-        System.out.println(RegexConstants.IF_STATEMENT);
-    }
-
-}
